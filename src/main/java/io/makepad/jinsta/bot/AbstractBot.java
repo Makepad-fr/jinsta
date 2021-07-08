5c3e0b16e7a611b3342d3275dd7fe0b457346be5
kaanyagci <kaan.yagci@clique.paris>
2021-07-09 01:04:42 +0200

    :fire: Private browsing removed
    Private browsing feature removed

--- src/main/java/io/makepad/jinsta/bot/AbstractBot.java
+++ src/main/java/io/makepad/jinsta/bot/AbstractBot.java
@@ -3,6 +3,7 @@ package io.makepad.jinsta.bot;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TimeoutException;
@@ -24,9 +25,6 @@ abstract class AbstractBot {
     FirefoxOptions options = new FirefoxOptions();
     String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36 OPR/60.0.3255.170";
     options.addPreference("general.useragent.override",userAgent);
-
-    options.addArguments("-private");
-    options.addPreference("browser.privatebrowsing.autostart", true);
     this.driver = new FirefoxDriver(options);
     this.wait = new WebDriverWait(this.driver, Duration.ofSeconds(120));
     this.driver.manage().window().maximize();
